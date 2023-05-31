09082cb9bdc810ac07c0b7ad5ff7112cfd3464e4
Guilherme Daniel <gui.dani13@gmail.com>
2023-05-31 18:09:31 -0300

    update visual nos botões da página inicial

--- app/components/LinkButton.tsx
+++ app/components/LinkButton.tsx
@@ -3,12 +3,14 @@ import Link from "next/link";
 type Props = {
   to: string;
   label: string;
+  icon?: any;
 };
 
-export default function LinkButton({ to, label }: Props) {
+export default function LinkButton({ to, label, icon }: Props) {
   return (
-    <Link href={to}>
-      <button className="btn btn-success hover:opacity-80">
+    <Link href={to} className="btn btn-lg btn-success hover:opacity-80 gap-2">
+        {icon}
+      <button>
         {label}
       </button>
     </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import LinkButton from "./components/LinkButton";
+import IconBook from "./components/icons/iconBook";
+import IconBuilding from "./components/icons/iconBuilding";
+import IconEgresso from "./components/icons/iconEgresso";
 
 export default async function Home() {
   return (
@@ -6,9 +9,9 @@ export default async function Home() {
       <h1 className="mt-4 mb-2 text-2xl">Painel Administrativo</h1>
       <hr />
       <div className="flex flex-col md:flex-row gap-4 mt-4">
-        <LinkButton to="/cursos" label="Cursos" />
-        <LinkButton to="/campus" label="Campus" />
-        <LinkButton to="/egressos" label="Egressos" />
+        <LinkButton to="/cursos" label="Cursos" icon={<IconBook />} />
+        <LinkButton to="/campus" label="Campus" icon={<IconBuilding />} />
+        <LinkButton to="/egressos" label="Egressos" icon={<IconEgresso />} />
       </div>
     </main>
   );
