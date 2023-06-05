c2779aa1d161b83b9f020e5a20feea155ea25f64
Guilherme Daniel <gui.dani13@gmail.com>
2023-06-05 10:56:19 -0300

    Trocado cor do botão

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <SignUp
+      appearance={{
+        elements: {
+          formButtonPrimary: "btn btn-success",
+        },
+      }}
+    />
+  );
 }
--- app/Providers/AuthProvider.tsx
+++ app/Providers/AuthProvider.tsx
@@ -1,6 +1,11 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import React from 'react'
+import { ClerkProvider } from "@clerk/nextjs";
+import { ptBR } from '@clerk/localizations'
+import React from "react";
 
-export default function AuthProvider({children}: {children: React.ReactNode}){
-  return <ClerkProvider>{children}</ClerkProvider>
+export default function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <ClerkProvider localization={ptBR}>{children}</ClerkProvider>;
 }
